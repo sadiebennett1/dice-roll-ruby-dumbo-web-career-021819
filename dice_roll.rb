47f150a319b7a82f07bb7fdaa9219f429821a2fa
blithe-hacker-0535 <sadiebenn1@gmail.com>
2019-02-07 17:41:14 +0000

    Automatically backed up by Learn

--- dice_roll.rb
+++ dice_roll.rb
@@ -3,6 +3,8 @@
 # Feel free to google "how to generate a random number in ruby"
 
 def roll
-  # code goes here
-  rand(1..6)
+  # # code goes here
+  return rand(1..6)
+  # nums = [1,2,3,4,5,6]
+  # return 
 end
